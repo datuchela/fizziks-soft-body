3ed8675595567ac71d4703c24f7148ebe5fc6e8f
datuchela <davit.narimanidze408@gmail.com>
2023-07-04 17:50:47 +0400

    feat: add unit property to Vector

--- src/engine/Vector.ts
+++ src/engine/Vector.ts
@@ -19,6 +19,24 @@ export class Vector {
     );
   }
 
+  get unit() {
+    let unitX = 0;
+    let unitY = 0;
+    if (this.x !== 0) {
+      unitX = this.x / Math.abs(this.x);
+    }
+    if (this.y !== 0) {
+      unitY = this.y / Math.abs(this.y);
+    }
+    return new Vector(unitX, unitY);
+  }
+
+  set unit(vector: Vector) {
+    throw new Error(
+      `Tried to set ${vector} as a unit vector, you cannot set unit vector explicitly.`
+    );
+  }
+
   add = (vector: Vector) => {
     return new Vector(this.x + vector.x, this.y + vector.y);
   };
@@ -27,8 +45,7 @@ export class Vector {
     return new Vector(this.x - vector.x, this.y - vector.y);
   };
 
-  scale = (n: number) => {
-    // ???
-    return new Vector(this.x * n, this.y * n);
+  scale = (scalar: number) => {
+    return new Vector(this.x * scalar, this.y * scalar);
   };
 }
