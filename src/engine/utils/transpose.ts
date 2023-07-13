aec8a60cbf97dbaaa279a27c9a095a505f8be55b
datuchela <davit.narimanidze408@ens.tsu.edu.ge>
2023-07-13 21:26:31 +0400

    refactor: organize names in transpose

--- src/engine/utils/transpose.ts
+++ src/engine/utils/transpose.ts
@@ -1,9 +1,13 @@
 export const transpose = <T>(array: T[][]) => {
   const transposedArray: T[][] = [];
 
-  for (let row = 0; row < array[0].length; ++row) {
+  // TODO: check shape
+  const initialRowLength = array[0].length;
+  const initialColumnLength = array.length;
+
+  for (let row = 0; row < initialRowLength; ++row) {
     const newRows: T[] = [];
-    for (let column = 0; column < array.length; ++column) {
+    for (let column = 0; column < initialColumnLength; ++column) {
       const value = array[column][row];
       newRows.push(value);
     }
