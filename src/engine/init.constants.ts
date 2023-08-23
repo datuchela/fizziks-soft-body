a63e4034022a0ecc1c3e65039a5a9e43dab5426a
datuchela <davit.narimanidze408@gmail.com>
2023-08-23 17:56:49 +0400

    fix: omit key name from "canvas" to "canvasSize"

--- src/engine/init.constants.ts
+++ src/engine/init.constants.ts
@@ -1,6 +1,6 @@
 import type { RequiredEngineConfig } from "./types";
 
-export const DEFAULT_ENGINE_CONFIG: Omit<RequiredEngineConfig, "canvas"> = {
+export const DEFAULT_ENGINE_CONFIG: Omit<RequiredEngineConfig, "canvasSize"> = {
   fps: 60,
   particles: {
     maxVelocity: Number.MAX_SAFE_INTEGER,
